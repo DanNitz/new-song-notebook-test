e2b74be68b4d53b8c279d5e1e10932505a73ab77
Dan Nitzken <48182329+DanNitz@users.noreply.github.com>
2021-12-16 11:16:14 -0500

    adding a songLibraryCtrls.js to pull my DOM buttons
    adding a songLibraryCtrls.js to pull my DOM buttons

---
+++ songLibraryCtrls.js
@@ -0,0 +1,17 @@
+//Dom Objects
+
+const allSongs = document.querySelector(".all-songs");
+const originals = document.querySelector(".originals");
+const covers = document.querySelector(".covers");
+const set1 = document.querySelector(".set-1");
+const set2 = document.querySelector(".set-2");
+const set3 = document.querySelector(".set-3");
+const set4 = document.querySelector(".set-4");
+const set5 = document.querySelector(".set-5");
+const otherSongs = document.querySelector(".other-songs");
+
+
+//const for my api to pull from 
+const baseApiUrl = "http://localhost:5000/api/";
+
+//
