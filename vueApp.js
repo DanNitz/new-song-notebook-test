e21d8c84dec647d6cb93210b6271842ef98281d7
Dan Nitzken <48182329+DanNitz@users.noreply.github.com>
2021-12-16 19:58:02 -0500

    quick update
    quick update

--- vueApp.js
+++
@@ -1,9 +0,0 @@
-const songDisplay = new Vue({
-    el: '#song-display',
-        data(){
-            return{
-                baseApiUrl:'http://localhost:5000/api/',
-                
-            }
-        }
-})
